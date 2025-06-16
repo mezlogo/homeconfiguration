dda256629f589e9f4b648de4b935cf9a51f7cd4e
Mezentsev Denis <mezentsev.d@olytechspb.com>
2025-06-16 22:36:12 +0300

    next

--- dotfiles/base_scripts/.local/bin/select-sink.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sinks=$(wpctl status | sed -n '/Sinks:/,/Sources:/p' | string replace -a '├' '' | string replace -a '─' '' | string replace -a '│' '' | string replace -a '└' '' | string match -r '.*\[vol:.*' | string replace -r '\s*\[vol:.*$' '' | string replace -r '^\s*(\d+)\.' '$1\t' | string replace -r '.*?\\*.*?(\\d+).*?(\\w.*$)' '<b>$1 $2 *</b>' | string replace -r '\s+' ' ')
-echo "$sinks"
