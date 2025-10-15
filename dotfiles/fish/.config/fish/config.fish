96ce81331280fb651369623c9b9a9686d1c2d0ef
Mezentsev Denis <mezentsev.d@olytechspb.com>
2025-10-15 23:59:32 +0300

    next

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -1,15 +1,17 @@
 if status is-interactive
-	set -g fish_greeting
+    set -g fish_greeting
 
-	set -g fish_key_bindings fish_vi_key_bindings
+    set -g fish_key_bindings fish_vi_key_bindings
 
-	set fzf_preview_dir_cmd eza --all --color=always
-	set fzf_preview_file_cmd bat
-	set fzf_fd_opts --hidden --max-depth 5
-	set fzf_diff_highlighter delta --paging=never --width=20
+    set fzf_preview_dir_cmd eza --all --color=always
+    set fzf_preview_file_cmd bat
+    set fzf_fd_opts --hidden --max-depth 5
+    set fzf_diff_highlighter delta --paging=never --width=20
 
-	bind -M insert shift-tab complete
-	bind -M insert tab complete-and-search
+    bind -M insert shift-tab complete
+    bind -M insert tab complete-and-search
+
+    eval "$(mise activate)"
 end
 
 fish_add_path -g ~/.local/bin
