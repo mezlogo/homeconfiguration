dbdb6d652326ded83bc6457d548cc6cc01faf16d
Mezentsev Denis <mezentsev.d@olytechspb.com>
2026-02-14 15:46:44 +0300

    next

--- dotfiles/nvim/.config/nvim/lua/myscratch.lua
+++ dotfiles/nvim/.config/nvim/lua/myscratch.lua
@@ -1,7 +1,6 @@
 local M = {}
 
-local function create_and_fill_buffer(lines, type)
-  local filetype = type or "text"
+local function create_and_fill_buffer(lines, filetype)
   local buf = vim.api.nvim_create_buf(true, false)
   vim.cmd('enew')
   vim.api.nvim_win_set_buf(0, buf)
@@ -13,10 +12,7 @@ local function create_and_fill_buffer(lines, type)
   vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
 end
 
-local function evaluate_and_show(opts)
-  -- Get the Lua code from command arguments
-  local lua_code = opts.args or ""
-
+local function evaluate_and_show(lua_code, filetype)
   -- Evaluate the Lua code and capture output
   local success, result = pcall(function()
     -- Use loadstring to evaluate the code and capture print output
@@ -58,7 +54,7 @@ local function evaluate_and_show(opts)
       for _, line in ipairs(result) do
         table.insert(lines, line)
       end
-      create_and_fill_buffer(lines, 'text')
+      create_and_fill_buffer(lines, filetype)
     else
       table.insert(lines, "(no output)")
     end
@@ -67,11 +63,30 @@ local function evaluate_and_show(opts)
   end
 end
 
+local function evaluate_and_show_json(opts)
+  local args = opts.args or ""
+  local code = 'vim.fn.json_encode(' .. args .. ')'
+  evaluate_and_show(code, 'json')
+end
+
+local function evaluate_and_show_text(opts)
+  local args = opts.args or ""
+  local code = args
+  evaluate_and_show(code, 'text')
+
+end
+
 M.setup = function ()
-  vim.api.nvim_create_user_command('MyScratch', evaluate_and_show, {
-    nargs = 2,           -- Requires exactly one argument
+  vim.api.nvim_create_user_command('MyScratch', evaluate_and_show_text, {
+    nargs = 1,           -- Requires exactly one argument
     complete = 'expression',  -- Enable expression completion
     desc = 'Evaluate Lua code and show output in scratch buffer'
+  })
+
+  vim.api.nvim_create_user_command('MyJson', evaluate_and_show_json, {
+    nargs = 1,           -- Requires exactly one argument
+    complete = 'expression',  -- Enable expression completion
+    desc = 'Evaluate Lua code, encode to json and show output in scratch buffer'
   })
 end
 
