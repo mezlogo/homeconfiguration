2ad4054c69435f088ced4f3a090255c1e02ce9f0
Mezentsev Denis <mezentsev.d@olytechspb.com>
2026-03-03 15:46:17 +0300

    next

--- dotfiles/nvim/.config/nvim/lua/plugins/guard.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/guard.lua
@@ -1,6 +1,14 @@
 return {
   "nvimdev/guard.nvim",
   config = function()
+    -- 1. Set global configuration to disable auto-format on save
+    vim.g.guard_config = {
+      fmt_on_save = false, -- Disable automatic formatting when saving a file
+      -- You can keep other defaults or add them here if needed
+      -- lsp_as_default_formatter = false,
+      -- save_on_fmt = true,
+      -- etc.
+    }
     local ft = require("guard.filetype")
     ft("json"):fmt({
       cmd = 'jq',
